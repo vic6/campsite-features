5223c24f1b527da9de71369fec87cec90b3c8aa7
vic6 <vicmatthews116@gmail.com>
2017-11-14 14:48:58 -0800

    Switch from live server to react dev server

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import preload from '../camp-features.js';
 
-const template = <p>Hello blah</p>;
+class Features extends React.Component {
+  render() {
+    console.log("hello");
+    return (
+      <div>
+        <h1>Campground Features</h1>
+        <ul>
+          <li>Toilet</li>
+          <li>Pets Allowed</li>
+          <li>Ice cream</li>
+          <li>Paper</li>
+          <li>Blah and sauce</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<Features />, document.getElementById("app"));
