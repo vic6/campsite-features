5dbeb139aa3def798b8d2989fe2c750fda460b8c
vic6 <vicmatthews116@gmail.com>
2017-11-19 23:26:46 -0800

    Add sass loaders and webpack config

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,6 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Feature from "./Feature.jsx";
 import Features from "./Features.jsx";
+import './styles/styles.scss';
 
 ReactDOM.render(<Features />, document.getElementById("app"));
