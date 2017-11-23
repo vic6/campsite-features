0ac241f9847847d0003840ad329208352ae95edb
vic6 <vicmatthews116@gmail.com>
2017-11-22 22:17:11 -0800

    Remove duplicate imports, refactor features into stateless component

--- src/Feature.jsx
+++ src/Feature.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 
 class Feature extends Component {
   constructor(props) {
--- src/Features.jsx
+++ src/Features.jsx
@@ -1,32 +1,23 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import Feature from "./Feature.jsx";
 import Header from "./Header.jsx";
 import { features } from "../camp-features.js";
 
-class Features extends Component {
-  constructor(props) {
-    super();
-  }
-  render() {
-    console.log(features);
-    return (
-      <div>
-        <Header title='Forrest Hills Campsite' />
-        <ul>
-          {features.map(feature => (
-            <Feature
-              key={feature.title}
-              title={feature.title}
-              presence={feature.presence}
-              subFeatures={feature.subfeatures}
-              icon={feature.icon}
-            />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const Features = () => (
+  <div>
+    <Header title="Forrest Hills Campsite" />
+    <ul>
+      {features.map(feature => (
+        <Feature
+          key={feature.title}
+          title={feature.title}
+          presence={feature.presence}
+          subFeatures={feature.subfeatures}
+          icon={feature.icon}
+        />
+      ))}
+    </ul>
+  </div>
+);
 
 export default Features;
